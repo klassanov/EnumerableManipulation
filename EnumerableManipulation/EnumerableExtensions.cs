1756502366c21fa94b14f9f6a5a4fddf8e42ef48
Alexander Klassanov <klassanov@gmail.com>
2023-02-01 17:58:17 +0200

    Add project files.

---
+++ EnumerableManipulation/EnumerableExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace EnumerableManipulation
+{
+    internal static class EnumerableExtensions
+    {
+        public static T GetMiddleElement<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            
+            IEnumerator<T> enumerator = collection.GetEnumerator();
+
+            if (!enumerator.MoveNext()) throw new ArgumentException($"{nameof(collection)} has no elements");
+
+            enumerator.Reset();
+
+
+            int length = 0;
+            bool secondIteration = false;
+            int i = 0;
+            T middleElement = default!;
+            bool found = false;
+
+            while (!found)
+            {
+                if (enumerator.MoveNext())
+                {
+                    if (secondIteration)
+                    {
+                        if (i == length / 2)
+                        {
+                            middleElement = enumerator.Current;
+                            found = true;
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        length++;
+                    }
+                }
+                else
+                {
+                    secondIteration = true;
+                    enumerator.Reset();
+                }
+            }
+
+            return middleElement;
+        }
+    }
+}
---
+++ EnumerableManipulation/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnumerableManipulation;
+
+var collection1 = new string[] { "To6ko" };
+var collection2 = new string[] { "To6ko", "Mitko" };
+var collection3 = new string[] { "To6ko", "Mitko", "Zai4o" };
+var collection4 = new string[] { "To6ko", "Mitko", "Zai4o", "Bai4o" };
+var collection5 = new string[] { "To6ko", "Mitko", "Zai4o", "Bai4o", "Val4o" };
+
+Console.WriteLine(collection1.GetMiddleElement()); //To6ko
+Console.WriteLine(collection2.GetMiddleElement()); //Mitko
+Console.WriteLine(collection3.GetMiddleElement()); //Mitko
+Console.WriteLine(collection4.GetMiddleElement()); //Zai4o
+Console.WriteLine(collection5.GetMiddleElement()); //Zai4o
